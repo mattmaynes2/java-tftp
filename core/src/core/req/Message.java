c56471844dd25ceb9cbad50bce1dd6d3a1468c7e
CJMatx <connor@localhost.(none)>
2016-01-21 12:13:51 -0500

    Finished the implementation of Message

--- core/src/core/req/Message.java
+++ core/src/core/req/Message.java
@@ -1,26 +1,30 @@
 package core.req;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 public abstract class Message {
 	
 	private OpCode opCode;
 	
-	public Message(byte[] bytes) {
-		
+	public Message(byte[] bytes) throws InvalidMessageException {
+		decode(bytes);
 	}
 	
 	public Message(OpCode opCode) {
 		this.opCode = opCode;
 	}
 	
-	protected void decode(byte[] bytes) {
+	protected void decode(byte[] bytes) throws InvalidMessageException{
+		if (bytes[0] != 0) {
+			throw new InvalidMessageException("The first byte of the opCode is not 0.");
+		}
 		
+		if (!OpCode.isValid(bytes[1])) {
+			throw new InvalidMessageException("The second byte of the opCode does not represent a valid message.");
+		}
+		
+		this.opCode = OpCode.convert(bytes[1]);
 	}
-	
-	@Override
-	public abstract String toString();
 	
 	public OpCode getOpCode() {
 		return this.opCode;
@@ -31,5 +35,9 @@ public abstract class Message {
 		out.write(0);
 		out.write(getOpCode().getCode());
 		return out.toByteArray();
+	}
+	
+	public String toString() {
+		return "OpCode: " + getOpCode().getCode();
 	}
 }
