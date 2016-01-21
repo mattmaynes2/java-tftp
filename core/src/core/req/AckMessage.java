33d3eaeee8c8530d53ba955aded9fd362bbd90db
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-01-21 13:18:04 -0500

    added Ack and Data Messages

---
+++ core/src/core/req/AckMessage.java
@@ -0,0 +1,54 @@
+package core.req;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import core.util.ByteUtils;
+
+
+public class AckMessage extends Message {
+
+	private int blockNum;
+	
+	public AckMessage(byte[] bytes) throws InvalidMessageException {
+		super(bytes);
+	}
+
+	public AckMessage(int blockNum){
+		super(OpCode.ACK);
+		this.blockNum=blockNum;
+	}
+	 
+	/**
+	 * Constructor used by classes that extend AckMessage
+	 */
+	protected AckMessage(OpCode opcode,int blockNum) {
+		super(opcode);
+		this.blockNum=blockNum;
+	}
+	@Override
+	public String toString() {
+		return super.toString()+" block number: "+this.blockNum;
+	}
+	
+	@Override
+	protected void decode(byte[] bytes) throws InvalidMessageException {
+		if (bytes.length<4){
+			throw new InvalidMessageException("Ack Message must be 4 bytes");
+		}
+		super.decode(bytes);
+		this.blockNum= ByteUtils.bytesToInt(Arrays.copyOfRange(bytes, 2, 4));
+	}
+	
+	@Override
+	public byte[] toBytes() {
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			out.write(super.toBytes());
+			out.write(ByteUtils.intToByteArray(blockNum));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return out.toByteArray();
+	}
+}
---
+++ core/src/core/req/DataMessage.java
@@ -0,0 +1,53 @@
+package core.req;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import core.util.ByteUtils;
+
+public class DataMessage extends AckMessage {
+
+	private byte[] data;
+	public DataMessage(byte[] bytes) throws InvalidMessageException {
+		super(bytes);
+	}
+	
+	public DataMessage (int blockNum, byte[] data) {
+		super(OpCode.DATA,blockNum);
+		this.data=data;
+	}
+
+	@Override
+	protected void decode(byte[] bytes) throws InvalidMessageException {
+		super.decode(bytes);
+		if(bytes.length>4) {
+			this.data=Arrays.copyOfRange(bytes, 4, bytes.length);
+		}else {
+			this.data=null;
+		}
+	}
+	
+	@Override
+	public byte[] toBytes() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			out.write(super.toBytes());
+			if(data!=null) {
+				out.write(data);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return out.toByteArray();
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + ByteUtils.bytesToHexString(this.data);
+	}
+	
+	public byte[] getData() {
+		return data;
+	}
+}
