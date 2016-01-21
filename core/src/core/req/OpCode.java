8773f671793d8a8fd1784a488c28bc6e8950c8c9
Matthew Maynes <matthew.maynes@yahoo.ca>
2016-01-21 11:08:53 -0500

    Added OpCode

---
+++ core/src/core/req/OpCode.java
@@ -0,0 +1,20 @@
+
+public enum OpCode {
+    READ    ((byte) 0x01),
+    WRITE   ((byte) 0x02),
+    DATA    ((byte) 0x03),
+    ACK     ((byte) 0x04),
+    ERROR   ((byte) 0x05);
+
+    private byte code;
+
+    OpCode (byte code) {
+        this.code = code;
+    }
+
+    public byte getCode(){
+        return this.code;
+    }
+
+
+}
