0614bbc8f7146b2d8270406a545d78d84a44574b
Connor <connormatthews@cmail.carleton.ca>
2016-01-26 12:22:03 -0500

    Added argument count property to CommandToken enum

--- core/src/core/cli/CommandToken.java
+++ core/src/core/cli/CommandToken.java
@@ -1,18 +1,24 @@
 package core.cli;
 
 public enum CommandToken {
-	READ		("read"),
-	WRITE		("write"),
-	SHUTDOWN	("shutdown"),
-	HELP		("help");
+	READ		("read", 1),
+	WRITE		("write", 1),
+	SHUTDOWN	("shutdown", 0),
+	HELP		("help", 0);
 	
 	private String token;
+	private int argc;
 	
-	CommandToken(String token) {
+	CommandToken(String token, int argc) {
 		this.token = token;
+		this.argc = argc;
 	}
 	
 	public String getToken() {
 		return this.token;
+	}
+	
+	public int getArgumentCount() {
+		return this.argc;
 	}
 }
