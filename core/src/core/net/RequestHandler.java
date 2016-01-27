d864971d44c0ee8df74b6daf0edc4ceaff8eba20
Cameron <ctblanch@gmail.com>
2016-01-27 18:28:08 -0500

    Fixed reads on server side

--- core/src/core/net/RequestHandler.java
+++ core/src/core/net/RequestHandler.java
@@ -1,9 +1,11 @@
 package core.net;
 
+import java.net.SocketAddress;
+
 import core.req.Request;
 
 public interface RequestHandler {
 
-    public void handleRequest (Request req);
+    public void handleRequest (Request req, SocketAddress requestAddress);
 
 }
