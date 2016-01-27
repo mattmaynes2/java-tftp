ed50e73bc3778fef032e8300bc951603930d5d18
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-01-27 17:21:53 -0500

    added errorSimulator class

--- errorSimulator/src/host/ErrorSimulator.java
+++ errorSimulator/src/host/ErrorSimulator.java
@@ -1,5 +1,31 @@
 package host;
 
-public class Host {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
+import core.req.Message;
+
+
+public class ErrorSimulator {
+
+	
+	public static void main(String[] args) {
+		int client_port;
+		DatagramSocket requestSocket;
+		try {
+			requestSocket = new DatagramSocket(68);
+			while(true){
+				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+				requestSocket.receive(packet);
+				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
