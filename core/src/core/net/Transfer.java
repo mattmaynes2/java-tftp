99484ed963a90feee00a5f18c2c4c382960e6d85
Matthew Maynes <matthew.maynes@yahoo.ca>
2016-01-28 13:54:20 -0500

    Added documentation to Transfer class

--- core/src/core/net/Transfer.java
+++ core/src/core/net/Transfer.java
@@ -8,46 +8,106 @@ import core.req.Message;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Transfer
+ *
+ * Runnable transfer moves a file between UDP endpoints.
+ * It is left to the subclass to use the appropriate notification function to
+ * signal transfer listeners of the different stages of the transfer.
+ */
 public abstract class Transfer implements Runnable {
 
-    private ArrayList<TransferListener> listeners;
+    /**
+     * Data block payload size. All datagram packet will contain
+     * this block size worth of bytes (not including the header bytes)
+     */
     public static final int BLOCK_SIZE = 512;
 
-    private String filename;
+    /**
+     * Listeners to the stages of a transfer
+     */
+    private ArrayList<TransferListener> listeners;
+
+    /**
+     * Socket that will be used to perform this transfer. This socket
+     * will be closed once the transfer is complete
+     */
     private NodeSocket socket;
 
+    /**
+     * Name of the file to transfer
+     */
+    private String filename;
+
+    /**
+     * Constructs a transfer with a socket which will move the specified file
+     *
+     * @param socket - Socket to send file over
+     * @param filename - Path of file to transfer
+     */
     public Transfer (NodeSocket socket, String filename){
         this.socket = socket;
         this.filename = filename;
         this.listeners = new ArrayList<TransferListener>();
     }
 
+    /**
+     * Returns the name of the file that is being transfered
+     *
+     * @return Name of file being transfered
+     */
     public String getFilename () {
         return this.filename;
     }
 
+    /**
+     * Returns the socket being used for this transfer
+     *
+     * @return The socket being used for this transfer
+     */
     public NodeSocket getSocket () {
         return this.socket;
     }
 
+    /**
+     * Adds a listener to this transfer
+     *
+     * @param listener - Listener to add to this transfer
+     */
     public void addTransferListener (TransferListener listener) {
         this.listeners.add(listener);
     }
 
+    /**
+     * Sends the initializing request to start this request
+     *
+     * @param filename - The name of the file to transfer
+     */
     public abstract void sendRequest(String filename) throws IOException;
 
+    /**
+     * Notifies all listeners that the transfer has started
+     */
     protected void notifyStart () {
         for (TransferListener listener : this.listeners) {
             listener.handleStart();
         }
     }
 
+    /**
+     * Notifies all listeners that a message has been received
+     *
+     * @param msg - Message that was received
+     */
     protected void notifyMessage (Message msg) {
         for (TransferListener listener : this.listeners) {
             listener.handleMessage(msg);
         }
     }
 
+    /**
+     * Notifies all listeners that the transfer has completed
+     */
     protected void notifyComplete () {
         for (TransferListener listener : this.listeners) {
             listener.handleComplete();
