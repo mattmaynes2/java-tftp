d24734d59f363e6df6327b9337fbe21c73990c88
Matthew Maynes <matthewmaynes1@gmail.com>
2016-01-27 22:05:58 -0500

    Fixed file streams so that they are closed when a transfer completes

--- core/src/core/net/Transfer.java
+++ core/src/core/net/Transfer.java
@@ -1,10 +1,28 @@
 package core.net;
 
+import core.net.NodeSocket;
+
 import java.io.IOException;
 
 public abstract class Transfer implements Runnable {
 
     public static final int BLOCK_SIZE = 512;
+
+    private String filename;
+    private NodeSocket socket;
+
+    public Transfer (NodeSocket socket, String filename){
+        this.socket = socket;
+        this.filename = filename;
+    }
+
+    public String getFilename () {
+        return this.filename;
+    }
+
+    public NodeSocket getSocket () {
+        return this.socket;
+    }
 
     public abstract void sendRequest(String filename) throws IOException;
 }
