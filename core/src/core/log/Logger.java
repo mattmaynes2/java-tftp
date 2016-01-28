45d51fb6302e93e6f605cbd3b57870c88fb0f3fe
Cameron <ctblanch@gmail.com>
2016-01-28 13:33:59 -0500

    Fixed logger

--- core/src/core/log/Logger.java
+++ core/src/core/log/Logger.java
@@ -1,5 +1,6 @@
 package core.log;
 import java.io.OutputStream;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.StreamHandler;
@@ -8,9 +9,12 @@ public class Logger {
 	
 	private static Level systemLogLevel = null;
 	
+	
 	public static synchronized void init(OutputStream stream, Level logLevel){
 		systemLogLevel = logLevel;
-		java.util.logging.Logger.getGlobal().addHandler(new StreamHandler(stream, new SimpleFormatter()));
+		ConsoleHandler handler = new ConsoleHandler();
+		handler.setLevel(logLevel);
+		java.util.logging.Logger.getGlobal().addHandler(handler);
 		java.util.logging.Logger.getGlobal().setLevel(logLevel);
 	}
 	
@@ -18,9 +22,7 @@ public class Logger {
 		if (systemLogLevel == null){
 			throw new UnitializedLoggerException("Logger has not been initialized");
 		}else{
-			java.util.logging.Logger logger = java.util.logging.Logger.getLogger(className);
-			logger.setLevel(systemLogLevel);
-			logger.log(logLevel, message);
+			java.util.logging.Logger.getGlobal().log(logLevel, message);
 		}
 	}
 }
