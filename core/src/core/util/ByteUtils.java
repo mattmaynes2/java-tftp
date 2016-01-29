10d17ac47ef3310728fc57506932817e1d7c3f26
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-01-29 02:01:40 -0500

    change how bytes are printed out and remove unused methods

--- core/src/core/util/ByteUtils.java
+++ core/src/core/util/ByteUtils.java
@@ -1,11 +1,9 @@
 
 package core.util;
 
-import java.security.InvalidParameterException;
 
 public class ByteUtils {
 
-	private final static int MAX_INT =65536; // maximum int that can be stored in 2 bytes
     public static int indexOf (byte[] array, byte value) {
         return ByteUtils.indexOf(array, 0, value);
     }
@@ -19,29 +17,13 @@ public class ByteUtils {
         return -1;
     }
     
-    public static int bytesToInt(byte[] bytes){
-    	if(bytes.length==2){
-    		int high= bytes[1]>=0? bytes[1]:256+bytes[1];
-    		int low =bytes[0]>=0? bytes[0]:256+bytes[0];
-    		return low |(high>>8);
-    	}
-    	throw new InvalidParameterException("Byte Array must be 2 bytes long");
-    }
-    
-    public static byte[] intToByteArray(int num){
-    	byte[] bytes=new byte[2];
-    	num=num%MAX_INT;
-    	bytes[0]=(byte)(num & 0xFF);
-    	bytes[1]=(byte)((num>>8) & 0xFF);
-    	return bytes;
-    }
-
 	public static String bytesToHexString(byte[] bytes) {
 		StringBuilder s= new StringBuilder();
 		for(byte b: bytes) {
-			s.append(Integer.toHexString(b));
+			s.append(Integer.toHexString(b)+", ");
 		}
-		return s.toString();
+		//remove trailing comma and space
+		return "["+s.substring(0, s.length()-2)+"]";
 	}
 
 }
