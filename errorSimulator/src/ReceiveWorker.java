9bba6daff53e9b1d70251493f093ea8cafcc30eb
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-01-29 01:30:39 -0500

    ErrorSimulator now shutsdown gracefully

---
+++ errorSimulator/src/ReceiveWorker.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+
+import core.log.Logger;
+import core.util.Worker;
+
+public class ReceiveWorker extends Worker {
+
+	private DatagramSocket requestSocket;
+	
+	public ReceiveWorker(int port) throws SocketException {
+		requestSocket = new DatagramSocket(port);
+	}
+	
+	@Override
+	public void execute() {
+         DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+         try {
+			requestSocket.receive(packet);
+			new SimulatorThread(packet).start();
+		} catch (SocketException e) {
+			// Ignore socket exception if not currently running
+			if(this.isRunning()) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {	
+			e.printStackTrace();
+		}
+         
+	}
+	
+	@Override
+	public void setup() {}
+
+	@Override
+	public void teardown() {
+		requestSocket.close();
+	}
+
+}
