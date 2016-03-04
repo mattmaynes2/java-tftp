d811a367ae77dc5883df8c1781be4b3fc4d5ded5
CJMatx <connormatthews@cmail.carleton.ca>
2016-03-04 11:09:34 -0500

    Added javadoc for SimulationEventListener

--- errorSimulator/src/threads/SimulationEventListener.java
+++ errorSimulator/src/threads/SimulationEventListener.java
@@ -2,9 +2,13 @@ package threads;
 
 public interface SimulationEventListener {
 
-	//Callback for a simulation completion
+	/**
+	 * Callback for a simulation completion
+	 */
 	public void simulationComplete();
 	
-	//Callback for the start of a simulation
+	/**
+	 * Callback for the start of a simulation
+	 */
 	public void simulationStarted();
 }
