7921c7c78c71e0713351443928e566f416e303be
Peter Mark <peter_mark12@hotmail.com>
2016-03-16 16:00:19 -0400

    removed unused imports

--- core/src/core/net/Transfer.java
+++ core/src/core/net/Transfer.java
@@ -1,23 +1,18 @@
 package core.net;
 
-import core.log.Logger;
-import core.net.NodeSocket;
-import core.net.TransferListener;
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
 
-import core.req.Message;
-import core.req.OpCode;
 import core.req.AckMessage;
 import core.req.ErrorCode;
 import core.req.ErrorMessage;
-import core.req.InvalidMessageException;
 import core.req.ErrorMessageException;
+import core.req.InvalidMessageException;
+import core.req.Message;
 import core.req.MessageOrderException;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.net.SocketAddress;
-import java.net.SocketException;
+import core.req.OpCode;
 
 /**
  * Transfer
